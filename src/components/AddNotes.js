9e896dc7023b377fb71ac9fa5f63cdd2c1ae3a11
Oksana Babychevska <oksana.babychevska@isr.de>
2022-11-06 20:43:38 +0100

    Add characterLimit

--- src/components/AddNotes.js
+++ src/components/AddNotes.js
@@ -3,14 +3,20 @@ import { useState } from "react"
 const AddNotes = ({ handleAddNote }) => {
     
     const [noteText, setNoteText] = useState('');
+    const characterLimit = 200;
 
-    const handleChange = (event) => {
-        setNoteText(event.target.value);
-    };
+	const handleChange = (event) => {
+		if (characterLimit - event.target.value.length >= 0) {
+			setNoteText(event.target.value);
+		}
+	};
 
-    const handleSaveClick = () => {
-        handleAddNote(noteText)
-    }
+const handleSaveClick = () => {
+		if (noteText.trim().length > 0) {
+			handleAddNote(noteText);
+			setNoteText('');
+		}
+	};
 
     return(
         <div className="note note-new">
@@ -22,7 +28,7 @@ const AddNotes = ({ handleAddNote }) => {
             onChange={handleChange}>
             </textarea>
             <div className="note-footer">
-                <small>200 Remaining</small>
+                <small>{characterLimit - noteText.length} Remaining</small>
                 <button className="save" onClick={handleSaveClick}>Save</button>
             </div>
         </div>
