fe04239bdd0732004a35fcbf1aa435c337205757
Oksana Babychevska <oksana.babychevska@isr.de>
2022-11-06 20:23:46 +0100

    AddNotes component

---
+++ src/components/AddNotes.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+const AddNotes = ({ handleAddNote }) => {
+    
+    const [noteText, setNoteText] = useState('');
+
+    const handleChange = (event) => {
+        setNoteText(event.target.value);
+    };
+
+    const handleSaveClick = () => {
+        handleAddNote(noteText)
+    }
+
+    return(
+        <div className="note note-new">
+            <textarea
+            row='8'
+            cols='10'
+            placeholder="Type to add a note..."
+            value={noteText}
+            onChange={handleChange}>
+            </textarea>
+            <div className="note-footer">
+                <small>200 Remaining</small>
+                <button className="save" onClick={handleSaveClick}>Save</button>
+            </div>
+        </div>
+    )
+}
+export default AddNotes
